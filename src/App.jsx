1b9afae9016e373cdd756f43768d24d756267c3f
Abhishek Kesharwani <abhishekkesharwani689@gmail.com>
2024-10-10 11:58:50 +0530

    use effect project

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    alert("I am on Loading the Page");
+  }, []);
+  useEffect(() => {
+    alert("I am use effect");
+  }, [count]);
+
+  return (
+    <>
+      <div></div>
+      <h1>Use Effect Example</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
